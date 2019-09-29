f43c5471fe6aabad93bfaf90b04e745361ee12ab
Michael Waldo <mbwcogic@yahoo.com>
2019-09-29 23:14:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,48 +1,14 @@
-def create_an_array
+def create_an_empty_array
+  
   []
+end
+
+def create_an_array
+  ["shaq", "kobe", "magic", "worthy"]
+end
+
+def add_element_to_end_of_array(array, fun)
+
+array.push = ("fun")
 end 
- 
-def 
   
-end
-
-def 
-  
-end
-
- 
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end 
- 
-def #remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
-
-def update_element_from_index(array, index_number, element)
-
-end
